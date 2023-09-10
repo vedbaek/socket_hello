574ce04e83c432054a078c05dd5f83724977af34
Buzz <hzhouw@foxmail.com>
2023-09-10 21:56:06 +0800

    openai

--- client.cpp
+++ client.cpp
@@ -1,56 +1,54 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-using namespace std;
 
 int main() {
-	// 1. 创建socket
-	// Protocal Family: AF_INET使用IPv4协议族，可选AF_INET6，即IPv6，和AF_UNIX本地通信
-	// Socket Type: SOCK_STREAM TCP流式socket，可选SOCK_DGRAM, UDP数据报，SOCK_RAW，可编辑IP报文
-	// Protocal: 0表示使用默认协议
-	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (client_socket == -1) {
-		cout << "Failed to create socket" << endl;
-		return 1;
-	}
-	cout << "Socket created" << endl;
+    // 创建Socket
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        std::cerr << "Failed to create socket.\n";
+        return 1;
+    }
 
-	// 2. 设置socket地址
-	sockaddr_in server_addr{};
-	int server_port = 8080;
-	std::string server_ip = "127.0.0.1";
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
-	server_addr.sin_port = htons(server_port);
+    // 设置服务器地址
+    sockaddr_in server_addr{};
+    int server_port = 8080;
+    std::string server_ip = "127.0.0.1";
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
+    server_addr.sin_port = htons(server_port);
 
-	// 3. 连接服务器
-	if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-		std::cerr << "Failed to connect to server" << endl;
-		return 1;
-	}
-	cout << "Connected to server" << endl;
+    // 连接服务器
+    if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        std::cerr << "Failed to connect to server.\n";
+        close(client_socket);
+        return 1;
+    }
 
-	// 4. 发送数据
-	string msg = "Hello from client!";
-	if (send(client_socket, msg.c_str(), msg.length(), 0) == -1) {
-		std::cerr << "Failed to send data: " << msg << endl;
-		return 1;
-	}
-	cout << "Message sent" << endl;
+    // 发送数据给服务器
+    std::string message = "Hello from client!";
+    if (send(client_socket, message.c_str(), message.length(), 0) == -1) {
+        std::cerr << "Failed to send data.\n";
+        close(client_socket);
+        return 1;
+    }
+    std::cout << "Sent message to server: " << message << "\n";
 
-	// 5. 等待响应
-	char buff[1024]{};
-	int len = recv(client_socket, buff, sizeof(buff), 0);
-	if (len == -1) {
-		std::cerr << "Failed to receive data" << endl;
-		close(client_socket);
-		return 1;
-	}
-	cout << "Message from server: " << buff << endl;
+    // 等待响应
+    char buffer[1024]{};
+    int len = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (len == -1) {
+        std::cerr << "Failed to receive data.\n";
+        close(client_socket);
+        return 1;
+    }
+    std::cout << "Received response from server: " << buffer << "\n";
 
-	// 6. 关闭socket
-	close(client_socket);
-	return 0;
+    // 关闭Socket
+    close(client_socket);
+    
+    return 0;
 }
-
--- server.cpp
+++ server.cpp
@@ -1,75 +1,73 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string>
-
-using namespace std;
 
 int main() {
-	// 1. 创建socket
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == -1) {
-		std::cerr << "Failed to create socket" << endl;
-		return 1;
-	}
-	cout << "Socket created!" << endl;
+    // 创建Socket
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        std::cerr << "Failed to create socket.\n";
+        return 1;
+    }
 
-	// 2. 设置socket地址
-	sockaddr_in server_addr{}, client_addr{};
-	int server_port = 8080;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(server_port);
+    // 设置Socket地址
+    sockaddr_in server_addr{}, client_addr{};
+    int server_port = 8080;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(server_port);
 
-	// 3. 绑定socket
-	if (bind(server_socket, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-		std::cerr << "Failed to bind socket" << endl;
-		close(server_socket);
-		return 1;
-	}
-	cout << "Socket binded!" << endl;
+    // 绑定Socket
+    if (bind(server_socket, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        std::cerr << "Failed to bind socket.\n";
+        close(server_socket);
+        return 1;
+    }
 
-	// 4. 开始监听
-	if (listen(server_socket, SOMAXCONN) == -1) {
-		std::cerr << "Failed to listen on socket" << endl;
-		return 1;
-	}
-	cout << "Listenning..." << endl;
+    // 开始监听
+    if (listen(server_socket, SOMAXCONN) == -1) {
+        std::cerr << "Failed to listen on socket.\n";
+        close(server_socket);
+        return 1;
+    }
+    std::cout << "Server is listening on port " << server_port << "\n";
 
-	// 5. 等待连接
-	socklen_t client_len = sizeof(client_addr);
-	int client_socket = accept(server_socket, (sockaddr*)&client_addr, &client_len);
-	if (client_socket == -1) {
-		std::cerr << "Failed to accept connection" << endl;
-		return 1;
-	}
-	cout << "Client accepted!" << endl;
+    // 等待连接
+    socklen_t client_len = sizeof(client_addr);
+    int client_socket = accept(server_socket, (sockaddr*)&client_addr, &client_len);
+    if (client_socket == -1) {
+        std::cerr << "Failed to accept connection.\n";
+        close(server_socket);
+        return 1;
+    }
 
-	// 6. 从客户端接收数据
-	char buff[1024]{};
-	int len = recv(client_socket, buff, sizeof(buff), 0);
-	if (len == -1) {
-		std::cerr << "Failed to receive data from client" << endl;
-		close(client_socket);
-		close(server_socket);
-		return 1;
-	}
-	cout << "Message from client: " << buff << endl;
+    // 从客户端接收数据
+    char buffer[1024]{};
+    int len = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (len == -1) {
+        std::cerr << "Failed to receive data.\n";
+        close(client_socket);
+        close(server_socket);
+        return 1;
+    }
+    std::cout << "Received message from client: " << buffer << "\n";
 
-	// 7. 发送响应给客户端
-	string msg = "Hello from server";
-	if (send(client_socket, msg.c_str(), msg.length(), 0) == -1) {
-		std::cerr << "Failed to send data to client" << endl;
-		close(client_socket);
-		close(server_socket);
-		return 1;
-	}
-	cout << "Message sent to client" << endl;
+    // 发送响应给客户端
+    std::string message = "Hello from server!";
+    if (send(client_socket, message.c_str(), message.length(), 0) == -1) {
+        std::cerr << "Failed to send data.\n";
+        close(client_socket);
+        close(server_socket);
+        return 1;
+    }
+    std::cout << "Sent response to client: " << message << "\n";
 
-	// 8. 关闭socket
-	close(client_socket);
-	close(server_socket);
+    // 关闭Socket
+    close(client_socket);
+    close(server_socket);
 
-	return 0;
+    return 0;
 }
