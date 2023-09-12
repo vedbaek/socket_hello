4c2f024ceba32ecf91d4000bd5c6f1b1cd05bff5
Vedbaek <hzhouw@126.com>
2023-09-12 20:35:09 +0800

    comment

--- server.cpp
+++ server.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <sys/socket.h>
+#include <sys/socket.h> // socket()、bind() 和 connect()
 #include <arpa/inet.h> // inet_pton() 和 inet_ntop() 函数，用于进行 IP 地址的二进制和字符串的转换
 #include <unistd.h> // close()，用于关闭文件描述符，包括套接字
 #include <string>
