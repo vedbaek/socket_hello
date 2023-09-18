ee17d573398c5684a8aa4be0a0b955dfd08d8d0b
Vedbaek <hzhouw@126.com>
2023-09-18 19:59:13 +0800

    getaddressinfo

--- getaddress_test.cpp
+++ getaddress_test.cpp
@@ -1,5 +1,19 @@
 #include <netdb.h> // addrinfo, getaddrinfo, freeaddrinfo
 #include <cstring> // memset
+#include <iostream>
+using namespace std;
+
+void print_sa_data(struct sockaddr *addr) {
+    struct sockaddr_in *addr_in = (struct sockaddr_in *)addr;
+    unsigned char *data = (unsigned char *)addr_in->sin_zero; // 或者使用 addr_in->sin_zero，取决于具体的结构体类型
+
+    printf("sa_data: ");
+    for (int i = 0; i < sizeof(addr_in->sin_zero); i++) {
+        printf("%02X ", data[i]);
+    }
+    printf("\n");
+}
+
 
 int main() {
     const char* hostname = "www.baidu.com";
@@ -14,13 +28,16 @@ int main() {
     if (status != 0) {
         // 获取地址信息失败
         // 处理错误情况
+        std::cerr << "getaddrinfo fail" << std::endl;
         return 1;
     }
+    cout << "getaddrinfo: " << status << endl;
 
     // 遍历结果链表
     struct addrinfo* p = result;
     while (p != nullptr) {
         // 可以访问p->ai_family、p->ai_addr等成员来获取具体的信息
+        print_sa_data(p->ai_addr);
         p = p->ai_next;
     }
 
