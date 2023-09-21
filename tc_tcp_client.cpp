45e0c0c3e1c7bbc85a26d425b01ad4452b2966c9
Vedbaek <hzhouw@126.com>
2023-09-21 13:24:15 +0800

    new tcp server client

--- tc_tcp_client.cpp
+++ tc_tcp_client.cpp
@@ -1,173 +1,85 @@
 #include <iostream>
 #include <cstring>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <vector>
-#include <map>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-#define SERVER_IP "127.0.0.1"
-#define PORT 12345
-
-// 消息类型枚举
-enum MessageType
-{
+// 结构体类型
+enum MessageType {
     PERSON,
     ANIMAL,
     CAR
 };
 
-struct MessageHeader
-{
-    MessageType type;
-    int size;
-};
-
-struct Person
-{
-    std::string name;
+// Person 结构体
+struct Person {
+    char name[20];
     int age;
 };
 
-struct Animal
-{
-    std::string species;
+// Animal 结构体
+struct Animal {
+    char species[20];
     float weight;
 };
 
-struct Car
-{
-    std::string brand;
+// Car 结构体
+struct Car {
+    char brand[20];
     int year;
 };
 
-int main()
-{
+int main() {
     int clientSocket;
-    struct sockaddr_in serverAddr;
+    struct sockaddr_in serverAddress;
 
     // 创建套接字
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1)
-    {
-        std::cerr << "创建套接字失败" << std::endl;
+    if (clientSocket == -1) {
+        std::cerr << "Failed to create socket" << std::endl;
         return -1;
     }
 
     // 设置服务器地址和端口
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // 服务器 IP 地址
+    serverAddress.sin_port = htons(8888); // 服务器端口号
 
-    // 将IPv4地址从文本转换为二进制形式
-    if (inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr) <= 0)
-    {
-        std::cerr << "转换服务器地址失败" << std::endl;
+    // 连接服务器
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to connect to server" << std::endl;
         return -1;
     }
 
-    // 连接到服务器
-    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-    {
-        std::cerr << "连接服务器失败" << std::endl;
-        return -1;
-    }
+    // 发送 Person 结构体
+    MessageType type = PERSON;
+    send(clientSocket, &type, sizeof(type), 0);
 
-    std::cout << "已连接到服务器" << std::endl;
+    Person person;
+    strncpy(person.name, "John", sizeof(person.name));
+    person.age = 30;
+    send(clientSocket, &person, sizeof(person), 0);
 
-    while (true)
-    {
-        // 发送消息给服务器
-        int choice;
-        std::cout << "请选择要发送的结构体类型：" << std::endl;
-        std::cout << "1. Person" << std::endl;
-        std::cout << "2. Animal" << std::endl;
-        std::cout << "3. Car" << std::endl;
-        std::cout << "0. 退出" << std::endl;
-        std::cout << "请输入选择：";
-        std::cin >> choice;
-        std::cin.ignore();
+    // 发送 Animal 结构体
+    type = ANIMAL;
+    send(clientSocket, &type, sizeof(type), 0);
 
-        if (choice == 0)
-        {
-            break;
-        }
+    Animal animal;
+    strncpy(animal.species, "Lion", sizeof(animal.species));
+    animal.weight = 200.5f;
+    send(clientSocket, &animal, sizeof(animal), 0);
 
-        // 构造消息头和对应的结构体数据
-        MessageHeader header;
-        char buffer[1024];
+    // 发送 Car 结构体
+    type = CAR;
+    send(clientSocket, &type, sizeof(type), 0);
 
-        switch (choice)
-        {
-        case 1:
-        {
-            // 构造 Person 结构体
-            Person person;
-            std::cout << "请输入姓名：";
-            std::getline(std::cin, person.name);
-            std::cout << "请输入年龄：";
-            std::cin >> person.age;
-            std::cin.ignore();
-
-            header.type = PERSON;
-            header.size = sizeof(person);
-            memcpy(buffer, &person, sizeof(person));
-            break;
-        }
-        case 2:
-        {
-            // 构造 Animal 结构体
-            Animal animal;
-            std::cout << "请输入物种：";
-            std::getline(std::cin, animal.species);
-            std::cout << "请输入重量：";
-            std::cin >> animal.weight;
-            std::cin.ignore();
-
-            header.type = ANIMAL;
-            header.size = sizeof(animal);
-            memcpy(buffer, &animal, sizeof(animal));
-            break;
-        }
-        case 3:
-        {
-            // 构造 Car 结构体
-            Car car;
-            std::cout << "请输入品牌：";
-            std::getline(std::cin, car.brand);
-            std::cout << "请输入年份：";
-            std::cin >> car.year;
-            std::cin.ignore();
-
-            header.type = CAR;
-            header.size = sizeof(car);
-            memcpy(buffer, &car, sizeof(car));
-            break;
-        }
-        default:
-            std::cout << "无效选择，请重新输入" << std::endl;
-            continue;
-        }
-
-        // 发送消息头和结构体数据给服务器
-        send(clientSocket, &header, sizeof(header), 0);
-        send(clientSocket, buffer, header.size, 0);
-
-        // 接收服务器响应
-        memset(&header, 0, sizeof(header));
-        int bytesRead = recv(clientSocket, &header, sizeof(header), 0);
-
-        if (bytesRead <= 0)
-        {
-            std::cerr << "与服务器的连接已断开" << std::endl;
-            break;
-        }
-
-        std::cout << "服务器响应：" << header.type << ", " << header.size << std::endl;
-    }
+    Car car;
+    strncpy(car.brand, "Toyota", sizeof(car.brand));
+    car.year = 2022;
+    send(clientSocket, &car, sizeof(car), 0);
 
     // 关闭客户端套接字
     close(clientSocket);
-    std::cout << "客户端套接字已关闭" << std::endl;
 
     return 0;
 }
--- ts_tcp_server.cpp
+++ ts_tcp_server.cpp
@@ -1,189 +1,134 @@
 #include <iostream>
 #include <cstring>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <vector>
-#include <map>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-#define PORT 12345
-
-// 消息类型枚举
-enum MessageType
-{
+// 结构体类型
+enum MessageType {
     PERSON,
     ANIMAL,
     CAR
 };
 
-struct MessageHeader
-{
-    MessageType type;
-    int size;
-};
-
-struct Person
-{
-    std::string name;
+// Person 结构体
+struct Person {
+    char name[20];
     int age;
 };
 
-struct Animal
-{
-    std::string species;
+// Animal 结构体
+struct Animal {
+    char species[20];
     float weight;
 };
 
-struct Car
-{
-    std::string brand;
+// Car 结构体
+struct Car {
+    char brand[20];
     int year;
 };
 
-void handleClient(int clientSocket)
-{
+void handleClient(int clientSocket) {
     char buffer[1024];
     int bytesRead;
 
-    while (true)
-    {
-        // 接收消息头
-        MessageHeader header;
-        memset(&header, 0, sizeof(header));
-        bytesRead = recv(clientSocket, &header, sizeof(header), 0);
+    while (true) {
+        MessageType type;
+        memset(&type, 0, sizeof(type));
+        bytesRead = recv(clientSocket, &type, sizeof(type), 0);
 
-        if (bytesRead <= 0)
-        {
+        if (bytesRead <= 0) {
             break;
         }
 
-        if (header.size > sizeof(buffer))
-        {
-            std::cerr << "数据大小超过缓冲区限制" << std::endl;
-            break;
-        }
-
-        // 接收结构体数据
-        memset(buffer, 0, sizeof(buffer));
-        bytesRead = recv(clientSocket, buffer, header.size, 0);
-
-        if (bytesRead <= 0)
-        {
-            break;
-        }
-
-        // 处理接收到的结构体数据
-        switch (header.type)
-        {
-        case PERSON:
-        {
-            if (bytesRead != sizeof(Person))
-            {
-                std::cerr << "接收到的 Person 结构体数据大小不匹配" << std::endl;
-                break;
-            }
-
+        if (type == PERSON) {
             Person person;
-            memcpy(&person, buffer, sizeof(person));
-            std::cout << "收到 Person 结构体消息：" << person.name << ", " << person.age << "岁" << std::endl;
-            break;
-        }
-        case ANIMAL:
-        {
-            if (bytesRead != sizeof(Animal))
-            {
-                std::cerr << "接收到的 Animal 结构体数据大小不匹配" << std::endl;
+            memset(&person, 0, sizeof(person));
+            bytesRead = recv(clientSocket, &person, sizeof(person), 0);
+
+            if (bytesRead <= 0) {
                 break;
             }
 
+            std::cout << "Received Person: " << person.name << ", " << person.age << " years old" << std::endl;
+        }
+        else if (type == ANIMAL) {
             Animal animal;
-            memcpy(&animal, buffer, sizeof(animal));
-            std::cout << "收到 Animal 结构体消息：" << animal.species << ", " << animal.weight << "kg" << std::endl;
-            break;
-        }
-        case CAR:
-        {
-            if (bytesRead != sizeof(Car))
-            {
-                std::cerr << "接收到的 Car 结构体数据大小不匹配" << std::endl;
+            memset(&animal, 0, sizeof(animal));
+            bytesRead = recv(clientSocket, &animal, sizeof(animal), 0);
+
+            if (bytesRead <= 0) {
                 break;
             }
 
+            std::cout << "Received Animal: " << animal.species << ", " << animal.weight << "kg" << std::endl;
+        }
+        else if (type == CAR) {
             Car car;
-            memcpy(&car, buffer, sizeof(car));
-            std::cout << "收到 Car 结构体消息：" << car.brand << ", " << car.year << std::endl;
-            break;
-        }
-        default:
-            std::cerr << "未知的消息类型" << std::endl;
-            break;
-        }
+            memset(&car, 0, sizeof(car));
+            bytesRead = recv(clientSocket, &car, sizeof(car), 0);
 
-        // 发送响应给客户端
-        send(clientSocket, &header, sizeof(header), 0);
+            if (bytesRead <= 0) {
+                break;
+            }
+
+            std::cout << "Received Car: " << car.brand << ", " << car.year << std::endl;
+        }
     }
 
-    // 关闭客户端连接
     close(clientSocket);
-    std::cout << "客户端连接已关闭" << std::endl;
 }
 
-int main()
-{
+int main() {
     int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrLen = sizeof(struct sockaddr_in);
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t clientAddressLength;
 
     // 创建套接字
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1)
-    {
-        std::cerr << "创建套接字失败" << std::endl;
+    if (serverSocket == -1) {
+        std::cerr << "Failed to create socket" << std::endl;
         return -1;
     }
 
     // 设置服务器地址和端口
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(PORT);
-
-    // 端口复用
-    int opt = 1;
-    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt, sizeof(opt));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(8888);
 
     // 绑定套接字到服务器地址和端口
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-    {
-        std::cerr << "绑定套接字失败" << std::endl;
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to bind socket" << std::endl;
         return -1;
     }
 
-    // 开始监听连接请求
-    if (listen(serverSocket, 5) < 0)
-    {
-        std::cerr << "开始监听连接请求失败" << std::endl;
+    // 监听连接请求
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Failed to listen on socket" << std::endl;
         return -1;
     }
 
-    std::cout << "等待客户端连接..." << std::endl;
+    std::cout << "Server started, waiting for connections..." << std::endl;
 
-    while (true)
-    {
-        // 接受客户端连接
-        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &addrLen);
-        if (clientSocket < 0)
-        {
-            std::cerr << "接受客户端连接失败" << std::endl;
+    // 接受连接请求并处理客户端
+    while (true) {
+        clientAddressLength = sizeof(clientAddress);
+        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+        if (clientSocket == -1) {
+            std::cerr << "Failed to accept client connection" << std::endl;
             continue;
         }
 
-        // 处理客户端连接
+        char clientIP[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIP, INET_ADDRSTRLEN);
+        std::cout << "Connected with client: " << clientIP << std::endl;
+
         handleClient(clientSocket);
     }
 
     // 关闭服务器套接字
     close(serverSocket);
-    std::cout << "服务器套接字已关闭" << std::endl;
 
     return 0;
 }
